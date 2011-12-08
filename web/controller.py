4c825524556ade1b32ac9f6f24988a28b38fb7d0
Thiago Santos <thiagossantos@gmail.com>
2011-12-07 23:09:19 -0300

    web: controller: incrementing web with template rendering

--- web/controller.py
+++ web/controller.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return 'Hello world!'
+def root():
+    return render_template('index.html')
+
+@app.route("/list/deputados")
+def list_deputados():
+    pass
 
 if __name__ == "__main__":
     app.run()
