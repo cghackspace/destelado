7df129ef0b035b1ba5b68bef7dcee69629d871cd
Paolo Victor <paolovictor@gmail.com>
2011-12-08 02:48:33 -0300

    Adicionada consulta de deputado por taxa de assiduidade

--- model/api.py
+++ model/api.py
@@ -4,6 +4,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 class DataAPI(object):
+    
+    CONSULTA_GET_POR_TX_ASSIDUIDADE = """select d.id, (CAST(SUM(a.presencas) as REAL) /
+                                                     CAST(SUM(a.presencas + a.faltas)
+                                                     as REAL)) as taxa
+                                       from deputado d, assiduidade a
+                                       where a.id_deputado = d.id
+                                       group by d.id order by taxa desc"""
 
     def __init__(self, db_url="sqlite:///test.db"):
         self.__engine__ = create_engine(db_url)
@@ -22,12 +29,28 @@ class DataAPI(object):
         if not gasto.descricao : raise "Descricao invalido"
         if not gasto.categoria : raise "Categoria invalida"
        
-
-    def get_deputados(self):
+    def get_deputados(self, order_by_assiduidade = False):
         return self.__session__.query(Deputado).all()
 
     def get_deputado_shallow(self, id):
         return self.__session__.query(Deputado).get(id)
+    
+    def get_deputados_por_assiduidade(self):
+        '''
+        Retorna uma lista com todos os deputados, ordenas por suas taxas de assiduidade,
+        onde a taxa de assiduidade eh TOTAL_PRESENCAS / (TOTAL_PRESENCAS + TOTAL_FALTAS).
+        Se um deputado nao possui registros de assiduidade, ele nao eh incluso no resultado
+        '''
+        resultset = self.__engine__.execute(DataAPI.CONSULTA_GET_POR_TX_ASSIDUIDADE)
+
+        deputados = []
+        for r in resultset:
+            deputado = self.get_deputado_shallow(r[0])
+            deputado.taxa_assiduidade = r[1]
+
+            deputados.append(deputado)
+
+        return deputados
 
     def get_deputado(self, id):
         deputado = self.__session__.query(Deputado).get(id)
@@ -94,7 +117,7 @@ class DataAPI(object):
         return gasto
 
     def inserir_assiduidade(self, assiduidade):
-        self.__session__.add(assiduidade)
+        self.__session__.merge(assiduidade)
         self.__session__.commit()
 
         return assiduidade
@@ -106,5 +129,15 @@ class DataAPI(object):
         return assiduidade
 
 if __name__ == '__main__':
-    print "Nothing to do..."
-    
+    import datetime
+
+    deputado = Deputado("Fulano", "PB", "???")
+
+    api = DataAPI()
+
+    deputado = api.inserir_deputado(deputado)
+    api.inserir_assiduidade(Assiduidade(deputado.id, datetime.date(2011, 1, 1), 1, 10))
+    api.inserir_assiduidade(Assiduidade(deputado.id, datetime.date(2011, 1, 1), 2, 11))
+
+    for d in api.get_deputados_por_assiduidade():
+        print d.id, d.nome, d.taxa_assiduidade
