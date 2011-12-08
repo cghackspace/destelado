497ca6ec1caafd8a890ac4e6d3852d527709c757
Thiago Santos <thiagossantos@gmail.com>
2011-12-08 00:01:16 -0300

    model:fakeapi: adding get_deputados function

--- model/fakeapi.py
+++ model/fakeapi.py
@@ -2,5 +2,15 @@ from model.entities import *
 
 def get_deputados():
     deputados = []
-    deputados.append( Deputado(id=1, nome='Teste 1', estado='PB', partido='PT') )
+    d = Deputado(nome='Teste 1', estado='PB', partido='PT')
+    d.id = 1
+    deputados.append( d )
+
+    d = Deputado(nome='Teste 2', estado='PB', partido='PMDB')
+    d.id = 2
+    deputados.append( d )
+
+    d = Deputado(nome='Teste 3', estado='PB', partido='PSOL')
+    d.id = 3
+    deputados.append( d )
     return deputados
