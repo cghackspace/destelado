5ee861cdaedc0bd43eadce00dadedeb2471e178a
Uian Sol <sol.uian@gmail.com>
2012-05-12 08:01:09 -0300

    upload 'deputados+gastos+assiduidades' to fusion tables

--- model/createtables.py
+++ model/createtables.py
@@ -78,6 +78,7 @@ def create_table_gastos(token):
     print table
     return table[8:-1]
 
+
 def insert_row_gastos(token, table_id, id_deputado, ano, descricao, categoria, valor):
     """Insere uma unica linhas na tabela Gastos, fornecendo dados"""
     sql = "insert into %s (id_deputado, ano, descricao, categoria, valor) VALUES (%d, %d, '%s', '%s', %d)" % (table_id, id_deputado, ano, descricao, categoria, valor)
@@ -86,6 +87,7 @@ def insert_row_gastos(token, table_id, id_deputado, ano, descricao, categoria, v
     row = _post(query, token)
     #print row
     return row    
+
 
 def insert_rows_gastos(token, table_id):
     """Insere todas as linhas na tabela Gastos, pegando do db local"""
@@ -122,14 +124,79 @@ def insert_rows_gastos(token, table_id):
         rows += _post(query, token)
         time.sleep(0.1)
 
-        print i
     #print rows
     return rows           
 
-token = getAuth()
+
+def create_table_assiduidade(token):
+    """Cria a tabela Assiduidade (colunas -> id_deputado|data|presencas|faltas)"""
+    sql = 'create table Assiduidade (id_deputado:NUMBER, data:DATETIME, presencas:NUMBER, faltas:NUMBER)'
+    sql = sql.encode('utf-8')
+    query = urllib.urlencode({'sql':sql})
+    table = _post(query, token)
+    print table
+    return table[8:-1]
+
+
+def insert_row_assiduidade(token, table_id, id_deputado, data, presencas, faltas):
+    """Insere uma unica linhas na tabela Assiduidade, fornecendo dados"""
+    sql = "insert into %s (id_deputado, data, presencas, faltas) VALUES (%d, '%s', %d, %d)" % (table_id, id_deputado, data, presencas, faltas)
+    sql = sql.encode('utf-8')
+    query = urllib.urlencode({'sql':sql})
+    row = _post(query, token)
+    #print row
+    return row    
+
+
+def insert_rows_assiduidade(token, table_id):
+    """Insere todas as linhas na tabela Assiduidade, pegando do db local"""
+    data = DataAPI()
+    deputados = data.get_deputados()
+
+    rows = ''
+    i = 0
+    while i < len(deputados):
+        sql = ''
+        count = 0
+        j = 0
+        while i + j < len(deputados):
+            if len(deputados[i+j].assiduidades) == 0:
+                j += 1
+                pass
+            if count + len(deputados[i+j].assiduidades) >= 500: break
+           
+            for k in xrange(len(deputados[i+j].assiduidades)):
+                id_deputado = int(deputados[i+j].assiduidades[k].id_deputado)
+                ano = int(deputados[i+j].assiduidades[k].data.year)
+                mes = deputados[i+j].assiduidades[k].data.month
+                dia = deputados[i+j].assiduidades[k].data.day
+                presencas = int(deputados[i+j].assiduidades[k].presencas)
+                faltas = int(deputados[i+j].assiduidades[k].faltas) 
+              
+                sql += ";insert into %s (id_deputado, data, presencas, faltas) VALUES (%d, '%d.%d.%d', %d, %d)" % (table_id, id_deputado, ano, mes, dia, presencas, faltas)
+                
+            count += len(deputados[i+j].assiduidades)
+            j += 1
+        i += j
+
+        sql = sql[1:].encode('utf-8')
+        query = urllib.urlencode({'sql':sql})
+        rows += _post(query, token)
+        time.sleep(0.1)
+
+    #print rows
+    return rows           
+
+
+
+### START UPLOAD ###
+#token = getAuth()
 #table_id = create_table_deputados(token)
 #ids_deputados = insert_rows_deputados(token, table_id)
 
-table_id = create_table_gastos(token)
-rows = insert_rows_gastos(token, table_id)
+#table_id = create_table_gastos(token)
+#rows = insert_rows_gastos(token, table_id)
+
+#table_id = create_table_assiduidade(token)
+#rows = insert_rows_assiduidade(token, table_id)
 #print rows
